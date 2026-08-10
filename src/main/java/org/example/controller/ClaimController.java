a1cbbbf135f27a556f64ff3f9922f365d8c27c25
lalit <lalit4java,jee16@gmail.com>
2026-08-10 17:45:00 +0530

    method test

--- src/main/java/org/example/controller/ClaimController.java
+++ src/main/java/org/example/controller/ClaimController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/claims")
+
+all methods to test
 public class ClaimController {
 
     private final ClaimService claimService;
